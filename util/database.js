59cd526e8e94d7d5605306e5fe98708635901cf1
Jose G <sgej93@gmail.com>
2022-04-01 22:48:34 -0500

    enable database with heroku

--- util/database.js
+++ util/database.js
@@ -11,12 +11,12 @@ const sequelize = new Sequelize({
   port: 5432,
   dialect: 'postgres',
   logging: false
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false
-  //   }
-  //}
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 
 });
 
